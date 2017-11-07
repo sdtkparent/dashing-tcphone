a0af14e853a6ed4b2c7fd04bb5389e7509eff379
sdtkparent <33357569+sdtkparent@users.noreply.github.com>
2017-11-07 17:33:59 -0600

    job

--- jobs/phonecallwaiting.rb
+++ jobs/phonecallwaiting.rb
@@ -1,7 +1,6 @@
 SCHEDULER.every '15m', :first_in => 0 do |job|
-	builds=config[:builds].map{|currentWaitingCalls|
-		currentOldest=JSON(get("http://iaonuccx01.sdt.local:9080/realtime/AgentCSQStats"))[0]['currentWaitingCalls']?'ok':'failing'
-		{:currentWaitingCalls => NumberWaiting}
+	builds=config[:builds].map{|currentWaitingCalls=JSON(get("http://iaonuccx01.sdt.local:9080/realtime/AgentCSQStats"))[0]['currentWaitingCalls']?'ok':'failing'
+		{:currentWaitingCalls => currentWaitingCalls}
 	}
 	failing_builds=builds.find_all{|build| build[:status]!='ok'}
 		send_event('travis_builds', {
